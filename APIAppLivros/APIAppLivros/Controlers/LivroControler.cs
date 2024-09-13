857c08569643c16f3d280dbc182fafd2e7e7cea5
Dan <danielfelipemg123@gmail.com>
2024-09-13 20:39:24 -0300

    Add files via upload

---
+++ APIAppLivros/APIAppLivros/Controlers/LivroControler.cs
@@ -0,0 +1,76 @@
+﻿using LivrosAPI.Models;
+using LivrosAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace LivrosAPI.Controllers
+{
+    [Route("api/livros")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livroRepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livroRepository = livroRepository;
+        }
+
+        // GET: api/livros/listar
+        [HttpGet]
+        [Route("listar")]
+        [SwaggerOperation(Summary = "Listar todos os livros", Description = "Este endpoint retorna uma listagem de todos os livros cadastrados.")]
+        public async Task<IEnumerable<Livro>> Listar()
+        {
+            return await _livroRepository.ListarTodosLivros();
+        }
+
+        // GET api/livros/detalhes/{id}
+        [HttpGet("detalhes/{id}")]
+        [SwaggerOperation(
+            Summary = "Obtém dados de um livro pelo ID",
+            Description = "Este endpoint retorna todos os dados de um livro cadastrado filtrando pelo ID.")]
+        public async Task<Livro> BuscarPorId(int id)
+        {
+            return await _livroRepository.BuscarPorId(id);
+        }
+
+        // POST api/livros
+        [HttpPost]
+        [SwaggerOperation(
+            Summary = "Cadastrar um novo Livro",
+            Description = "Este endpoint é responsável por cadastrar um novo livro no banco.")]
+        public async Task<IActionResult> Post([FromBody] Livro dados)
+        {
+            var livroExistente = await _livroRepository.BuscarPorTitulo(dados.Titulo);
+            if (livroExistente != null)
+                return BadRequest("Um livro com esse título já existe.");
+
+            await _livroRepository.Salvar(dados);
+            return Ok();
+        }
+
+        // PUT api/livros/{id}
+        [HttpPut("{id}")]
+        [SwaggerOperation(
+            Summary = "Atualizar os dados de um livro filtrando pelo ID",
+            Description = "Este endpoint é responsável por atualizar os dados de um livro no banco.")]
+        public async Task<IActionResult> Put(int id, [FromBody] Livro dados)
+        {
+            dados.Id = id;
+            await _livroRepository.Atualizar(dados);
+            return Ok();
+        }
+
+        // DELETE api/livros/{id}
+        [HttpDelete("{id}")]
+        [SwaggerOperation(
+            Summary = "Remover um livro filtrando pelo ID",
+            Description = "Este endpoint é responsável por remover os dados de um livro no banco.")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _livroRepository.DeletarPorId(id);
+            return Ok();
+        }
+    }
+}
---
+++ APIAppLivros/APIAppLivros/Repositories/LivroRepository.cs
@@ -0,0 +1,75 @@
+﻿using LivrosAPI.Models;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace LivrosAPI.Repositories
+{
+    public class LivroRepository
+    {
+        private readonly string _connectionString;
+
+        public LivroRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        private IDbConnection Connection =>
+            new MySqlConnection(_connectionString);
+
+        public async Task<IEnumerable<Livro>> ListarTodosLivros()
+        {
+            var sql = "SELECT * FROM livros";
+            using (var conn = Connection)
+            {
+                return await conn.QueryAsync<Livro>(sql);
+            }
+        }
+
+        public async Task<Livro> BuscarPorId(int id)
+        {
+            var sql = "SELECT * FROM livros WHERE Id = @Id";
+            using (var conn = Connection)
+            {
+                return await conn.QueryFirstOrDefaultAsync<Livro>(sql, new { Id = id });
+            }
+        }
+
+        public async Task<Livro> BuscarPorTitulo(string titulo)
+        {
+            var sql = "SELECT * FROM livros WHERE Titulo = @Titulo";
+            using (var conn = Connection)
+            {
+                return await conn.QueryFirstOrDefaultAsync<Livro>(sql, new { Titulo = titulo });
+            }
+        }
+
+        public async Task<int> DeletarPorId(int id)
+        {
+            var sql = "DELETE FROM livros WHERE Id = @Id";
+            using (var conn = Connection)
+            {
+                return await conn.ExecuteAsync(sql, new { Id = id });
+            }
+        }
+
+        public async Task<int> Salvar(Livro dados)
+        {
+            var sql = "INSERT INTO livros (Titulo, Autor, AnoPublicacao, Genero, NumeroPaginas) " +
+                      "VALUES (@Titulo, @Autor, @AnoPublicacao, @Genero, @NumeroPaginas)";
+            using (var conn = Connection)
+            {
+                return await conn.ExecuteAsync(sql, dados);
+            }
+        }
+
+        public async Task<int> Atualizar(Livro dados)
+        {
+            var sql = "UPDATE livros SET Titulo = @Titulo, Autor = @Autor, AnoPublicacao = @AnoPublicacao, " +
+                      "Genero = @Genero, NumeroPaginas = @NumeroPaginas WHERE Id = @Id";
+            using (var conn = Connection)
+            {
+                return await conn.ExecuteAsync(sql, dados);
+            }
+        }
+    }
+}
